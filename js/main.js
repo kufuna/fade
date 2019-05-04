e23aa949a5f03efcaffe51ee50da43a670fd68e9
dubrovsky <33762730+kufuna@users.noreply.github.com>
2019-05-04 17:30:39 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function offset(el) {
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
 
-window.addEventListener('scroll',function(){
+window.addEventListener('touchmove',function(){
 	for (var i = 0; i < a.length; i++) {
 		if (window.scrollY > offset(a[i]).top) {
 			var differance = window.scrollY - offset(a[i]).top;
