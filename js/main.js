ec25b33b2bf3a095424170a8314aacc9a4a35fa3
dubrovsky <33762730+kufuna@users.noreply.github.com>
2019-05-04 17:25:15 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+
+var a = document.querySelectorAll('.box');
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+window.addEventListener('scroll',function(){
+	for (var i = 0; i < a.length; i++) {
+		if (window.scrollY > offset(a[i]).top) {
+			var differance = window.scrollY - offset(a[i]).top;
+			var percentage = differance / a[i].clientHeight;
+			console.log(percentage)
+			a[i].style.opacity = percentage
+		}
+	}
+})
+
+
+}
